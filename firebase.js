a9a34e2002d607dedd53241c834138f7ddfff3e4
Miray Sıla Gürbüz <106536727+MiraySilaGurbuz@users.noreply.github.com>
2025-06-26 16:41:46 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword  } from 'firebase/auth';
 
 import { getDatabase } from 'firebase/database';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDeS0Nc_4gjPD_ZGP7Rf5v4Jp3mMl8AJK8",
-  authDomain: "scrabbleyeni.firebaseapp.com",
-  projectId: "scrabbleyeni",
-  storageBucket: "scrabbleyeni.firebasestorage.app",
-  messagingSenderId: "47993579802",
-  appId: "1:47993579802:web:2f9cced9ac56a66ed4e4a3"
+  apiKey: "your_apiKey",
+  authDomain: "your_authDomain.firebaseapp.com",
+  projectId: "your_projectId",
+  storageBucket: "your_storageBucket.firebasestorage.app",
+  messagingSenderId: "your_messagingSenderId",
+  appId: "your_appId"
 };
 
 // Initialize Firebase
@@ -25,5 +21,5 @@ if (!getApps().length) {
 }
 const auth = getAuth(app);
 
-const db = getDatabase(app); // ✅ EKLENDİ
+const db = getDatabase(app);
 export { auth, db };
