4fc13a7c462c2fea2cd8592598810b2e66996967
Miray Sıla Gürbüz <106536727+MiraySilaGurbuz@users.noreply.github.com>
2025-06-26 16:45:54 +0300

    Update gameService.js

--- services/gameService.js
+++ services/gameService.js
@@ -7,7 +7,7 @@ import {getActiveLettersForPlayer} from "../services/LetterService"
 import { ref, update } from "firebase/database";
 import { db } from "../firebase";
 
-const DATABASE_URL = "https://scrabbleyeni-default-rtdb.firebaseio.com";
+const DATABASE_URL = "your_url";
 
 export async function getAllGames(playerId) {
   try {
